1c5284fc9a3c7fefe0d8024c94d645234e983b63
Shubham Hibare <shubham@hibare.in>
2026-07-05 15:47:37 +0530

    Revert "fix: add CONCURRENTLY to DROP/CREATE INDEX statements in migration"
    This reverts commit 73e505bdf34c57585cad079d9b3db94247b04fe3.

--- internal/db/migrations/table/02_api_keys.down.sql
+++ internal/db/migrations/table/02_api_keys.down.sql
@@ -1,9 +1,9 @@
 -- Down Migration: Drop api_keys table and indexes
 
-DROP INDEX CONCURRENTLY IF EXISTS idx_api_keys_revoked_at;
-DROP INDEX CONCURRENTLY IF EXISTS idx_api_keys_expires_at;
-DROP INDEX CONCURRENTLY IF EXISTS idx_api_keys_key_hash;
-DROP INDEX CONCURRENTLY IF EXISTS idx_api_keys_user_id;
-DROP INDEX CONCURRENTLY IF EXISTS idx_api_keys_state;
+DROP INDEX IF EXISTS idx_api_keys_revoked_at;
+DROP INDEX IF EXISTS idx_api_keys_expires_at;
+DROP INDEX IF EXISTS idx_api_keys_key_hash;
+DROP INDEX IF EXISTS idx_api_keys_user_id;
+DROP INDEX IF EXISTS idx_api_keys_state;
 
 DROP TABLE IF EXISTS api_keys;
